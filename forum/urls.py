631781169ce5560c7a89b9d0ca0dee2476373193
arrancardnell <arrancardnell@gmail.com>
2016-07-17 13:48:43 +0100

    Added password change form and fixed string imports on urls.

--- forum/urls.py
+++ forum/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
     # login views
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^logout-then-login/$',
-        'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
+        auth_views.logout_then_login, name='logout_then_login'),
+
+    # password views
+    url(r'^password-change/$', auth_views.password_change, name='password_change'),
+    url(r'^password-change/done/$', auth_views.password_change_done, name='password_change_done'),
 
     # section views
     url(r'^$', views.section_list, name='section_list'),
