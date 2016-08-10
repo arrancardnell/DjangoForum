16dabfc0c88ae05ee8d77908c9077c0e84b7cfb8
arrancardnell <arrancardnell@gmail.com>
2016-08-10 20:46:39 +0100

    Added jump menu.

--- forum/templatetags/forum_tags.py
+++ forum/templatetags/forum_tags.py
@@ -1,12 +1,20 @@
+from collections import OrderedDict
 from django import template
 
 register = template.Library()
 
-@register.simple_tag(takes_context=True)
+
+@register.inclusion_tag('forum/jump_menu.html', takes_context=True)
 def jump_menu(context):
-    jump_menu = {}
+    jump_menu = OrderedDict([('jumps', {})])
+    jumps = OrderedDict()
     request = context['request']
-    path = str(request.path)
-    path_list = path.split('/')
-    
-    return path_list[1:-1]
+    path = str(request.path).strip('/')  # forum/general/welcome
+    path_list = path.split('/')  # ['forum', 'general', 'welcome']
+
+    for index, jump_name in enumerate(path_list):
+        link = '/' + '/'.join(path_list[:index + 1]) + '/'   # /forum/general/welcome/
+        jumps.setdefault(jump_name, link)
+    jump_menu['jumps'].update(jumps)
+
+    return jump_menu
